de0dec741803c1ea780ad8fbdbb277214832a6a2
jvitorthomaz <jovitorthomaz2@gmail.com>
2024-06-04 20:07:41 -0300

    test: Working with widget tests part.2

--- flutter_curso_tests/test/home_test.dart
+++ flutter_curso_tests/test/home_test.dart
@@ -1,3 +1,4 @@
+import 'package:estilizacao_componentes/components/box_card.dart';
 import 'package:estilizacao_componentes/data/bank_inherited.dart';
 import 'package:estilizacao_componentes/screens/home.dart';
 import 'package:flutter/material.dart';
@@ -31,4 +32,33 @@ void main(){
     expect(find.byKey(Key('testKey')), findsOneWidget);
   });
 
+  testWidgets('finds 5 BoxCards', (tester) async{
+    await tester.pumpWidget(
+      MaterialApp(
+        home: BankInherited(child: Home()),
+      )
+    );
+    expect(find.byWidgetPredicate((widget) {
+      if (widget is BoxCard) {
+        return true;
+      } else {
+        return false;
+      }
+
+    }), findsNWidgets(5));
+  });
+
+  testWidgets('When tap Deposit should update earned in 10 ', (tester) async{
+    await tester.pumpWidget(
+      MaterialApp(
+        home: BankInherited(child: Home()),
+      )
+    );
+
+    await tester.tap(find.text('Deposit'));
+    await tester.tap(find.text('Earned'));
+    await tester.pumpAndSettle();
+
+    expect(find.text('\$10.0'), findsOneWidget);
+  });
 }
