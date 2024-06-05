77bec26be43e86aad45603d524c7b834e4980c9c
jvitorthomaz <jovitorthomaz2@gmail.com>
2024-06-05 20:28:42 -0300

    test: integration test - testing with provider and context

--- flutter_testes_de_integração/integration_test/app_test.dart
+++ flutter_testes_de_integração/integration_test/app_test.dart
@@ -1,13 +1,18 @@
+import 'package:client_control/models/clients.dart';
+import 'package:client_control/models/types.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:client_control/main.dart' as app;
+import 'package:provider/provider.dart';
 
 void main(){
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets('Integration Test', (tester) async{
-    app.main();
+    final providerKey = GlobalKey();
+    app.main([], providerKey);
+
     await tester.pumpAndSettle();
 
     // testando fluxo do app
@@ -52,31 +57,38 @@ void main(){
     expect(find.text('Ferro'), findsOneWidget);
     expect(find.byIcon(Icons.card_giftcard), findsOneWidget);
 
+    expect(Provider.of<Types>(providerKey.currentContext!, listen: false).types.last.name, 'Ferro');
+    expect(Provider.of<Types>(providerKey.currentContext!, listen: false).types.last.icon, Icons.card_giftcard);
+
     // Testando criação de novo cliente
-      await tester.tap(find.byIcon(Icons.menu));
-      await tester.pumpAndSettle();
+    await tester.tap(find.byIcon(Icons.menu));
+    await tester.pumpAndSettle();
 
-      await tester.tap(find.text('Gerenciar clientes'));
-      await tester.pumpAndSettle();
+    await tester.tap(find.text('Gerenciar clientes'));
+    await tester.pumpAndSettle();
 
-      await tester.tap(find.byType(FloatingActionButton));
-      await tester.pumpAndSettle();
+    await tester.tap(find.byType(FloatingActionButton));
+    await tester.pumpAndSettle();
 
-      await tester.enterText(find.byKey(Key('NameKey1')), 'User Test');
-      await tester.enterText(find.byKey(Key('EmailKey1')), 'user_test@bot.com.br');
+    await tester.enterText(find.byKey(Key('NameKey1')), 'User Test');
+    await tester.enterText(find.byKey(Key('EmailKey1')), 'user_test@bot.com.br');
 
-      await tester.tap(find.byIcon(Icons.arrow_downward));
-      await tester.pumpAndSettle();
+    await tester.tap(find.byIcon(Icons.arrow_downward));
+    await tester.pumpAndSettle();
 
-      await tester.tap(find.text('Ferro').last);
-      await tester.pumpAndSettle();
+    await tester.tap(find.text('Ferro').last);
+    await tester.pumpAndSettle();
 
-      await tester.tap(find.text('Salvar'));
-      await tester.pumpAndSettle();
+    await tester.tap(find.text('Salvar'));
+    await tester.pumpAndSettle();
 
-      //Verificando se cliente foi cadastrado corretamente
-      expect(find.text('User Test (Ferro)'), findsOneWidget);
-      expect(find.byIcon(Icons.card_giftcard), findsOneWidget);
+    //Verificando se cliente foi cadastrado corretamente
+    expect(find.text('User Test (Ferro)'), findsOneWidget);
+    expect(find.byIcon(Icons.card_giftcard), findsOneWidget);
+
+    expect(Provider.of<Clients>(providerKey.currentContext!, listen: false).clients.last.name, 'User Test');
+    expect(Provider.of<Clients>(providerKey.currentContext!, listen: false).clients.last.email, 'user_test@bot.com.br');
+
   });
 
 }
